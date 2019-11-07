015e0785ac0f5c6218dc9d93167d4819dfe08a9d
Chris Colvard <cjcolvar@indiana.edu>
2019-11-07 16:56:21 -0500

    [WIP] Add dialog for about text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,52 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
 import './Footer.scss';
 
-const Footer = () => (
-  <Grid container className="footer">
-    <Grid item xs={6}>
-      <Typography variant="body1">&copy; Indiana University, 2018</Typography>
+function Footer() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Grid container className="footer">
+      <Grid item xs={6}>
+        <Typography variant="body1">&copy; Indiana University, 2018</Typography>
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        style={{
+          textAlign: 'right',
+        }}
+      >
+        <Typography variant="body1">
+          <Button variant="outlined" color="secondary" onClick={handleClickOpen}>
+            About Timeliner
+          </Button>
+          <Dialog onClose={handleClose} aria-labelledby="about-timeliner" open={open}>
+            <MuiDialogTitle id="about-timeliner" onClose={handleClose}>
+              About Timeliner
+            </MuiDialogTitle>
+            <MuiDialogContent dividers>
+              <Typography>
+                Some text here.
+              </Typography>
+            </MuiDialogContent>
+          </Dialog>
+        </Typography>
+      </Grid>
     </Grid>
-    <Grid
-      item
-      xs={6}
-      style={{
-        textAlign: 'right',
-      }}
-    >
-      <Typography variant="body1">About Timeliner | Help | Contact</Typography>
-    </Grid>
-  </Grid>
-);
+  );
+}
 
 export default Footer;
