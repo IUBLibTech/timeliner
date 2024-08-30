66a092d27e0cfc0d0357578c5483302694df14a1
Stephen Fraser <stephen.fraser@digirati.com>
2024-08-30 17:46:22 +0100

    Updated deps

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  esbuild: {
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+
+  // config options
+  plugins: [react()],
+});
