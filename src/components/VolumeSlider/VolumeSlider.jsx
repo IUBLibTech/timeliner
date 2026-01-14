1c516e54fc42e1c5d176728a956fa78ec686dcb4
dwithana <dwithana@iu.edu>
2026-01-14 13:05:51 -0800

    Fix a11y in MUI v3 slider & video player and fix broken volume change handler

--- src/components/VolumeSlider/VolumeSlider.jsx
+++ src/components/VolumeSlider/VolumeSlider.jsx
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Slider from '@material-ui/lab/Slider';
 import VolumeDown from '@material-ui/icons/VolumeDown';
 import VolumeUp from '@material-ui/icons/VolumeUp';
+import useVolumeSlider from '../../hooks/useVolumeSlider';
 import './VolumeSlider.scss';
 
 const SPEAKER_ICON_SIZE = {
@@ -10,39 +11,38 @@ const SPEAKER_ICON_SIZE = {
   height: 20,
 };
 
-class VolumeSlider extends Component {
-  static propTypes = {
-    /** Current volume value */
-    volume: PropTypes.number.isRequired,
-    /** Handler for when volume is changed */
-    onVolumeChanged: PropTypes.func.isRequired,
-  };
-  static defaultProps = {
-    volume: 100,
-  };
+function VolumeSlider({ volume, onVolumeChanged }) {
+  const { containerRef, onVolumeInputChange } = useVolumeSlider(volume, onVolumeChanged);
 
-  onVolumeInputChange = (ev, value) => {
-    const { onVolumeChanged } = this.props;
-    if (onVolumeChanged) {
-      onVolumeChanged(parseInt(value, 10));
-    }
-  };
-
-  render() {
-    const { volume } = this.props;
-    return (
-      <div className="volume-slider">
-        <VolumeDown color="disabled" style={SPEAKER_ICON_SIZE} />
-        <Slider
-          min={0}
-          max={100}
-          value={volume}
-          onChange={this.onVolumeInputChange}
-        />
-        <VolumeUp color="disabled" style={SPEAKER_ICON_SIZE} />
-      </div>
-    );
-  }
+  return (
+    <div
+      ref={containerRef}
+      className="volume-slider"
+      role="group"
+      aria-label="Volume control"
+    >
+      <VolumeDown color="disabled" style={SPEAKER_ICON_SIZE} aria-hidden="true" />
+      <Slider
+        min={0}
+        max={100}
+        value={volume}
+        onChange={onVolumeInputChange}
+        aria-label="Volume"
+      />
+      <VolumeUp color="disabled" style={SPEAKER_ICON_SIZE} aria-hidden="true" />
+    </div>
+  );
 }
+
+VolumeSlider.propTypes = {
+  /** Current volume value */
+  volume: PropTypes.number.isRequired,
+  /** Handler for when volume is changed */
+  onVolumeChanged: PropTypes.func.isRequired,
+};
+
+VolumeSlider.defaultProps = {
+  volume: 100,
+};
 
 export default VolumeSlider;
--- src/components/VolumeSliderCompact/VolumeSliderCompact.jsx
+++ src/components/VolumeSliderCompact/VolumeSliderCompact.jsx
@@ -1,85 +1,90 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import './VolumeSliderCompact.scss';
 import PropTypes from 'prop-types';
 import VolumeUp from '@material-ui/icons/VolumeUp';
 import VolumeDown from '@material-ui/icons/VolumeDown';
 import VolumeOff from '@material-ui/icons/VolumeOff';
 import Slider from '@material-ui/lab/Slider';
+import useVolumeSlider from '../../hooks/useVolumeSlider';
 
 const SPEAKER_ICON_SIZE = {
   width: 20,
   height: 20,
 };
 
-class VolumeSliderCompact extends Component {
-  state = { previousVolume: null };
+function VolumeSliderCompact({ volume, onVolumeChanged, flipped, disabled }) {
+  const [previousVolume, setPreviousVolume] = useState(null);
 
-  static propTypes = {
-    /** Current volume value */
-    volume: PropTypes.number.isRequired,
-    /** Handler for when volume is changed */
-    onVolumeChanged: PropTypes.func.isRequired,
-    /** Flip the order of the slider and icon */
-    flipped: PropTypes.bool,
-    disabled: PropTypes.bool,
-  };
+  const { containerRef, onVolumeInputChange } = useVolumeSlider(
+    volume,
+    onVolumeChanged,
+    { muteButtonSelector: '.volume-slider-compact__muter' }
+  );
 
-  static defaultProps = {
-    volume: 100,
-    flipped: false,
-  };
-
-  onVolumeInputChange = (ev, value) => {
-    const { onVolumeChanged } = this.props;
-    if (onVolumeChanged) {
-      onVolumeChanged(parseInt(value, 10));
-    }
-  };
-
-  onToggle = () => {
-    const { volume, onVolumeChanged } = this.props;
-    const { previousVolume } = this.state;
+  const onToggle = () => {
     if (onVolumeChanged) {
       if (volume === 0) {
         onVolumeChanged(previousVolume || 100);
       } else {
-        this.setState({ previousVolume: volume });
+        setPreviousVolume(volume);
         onVolumeChanged(0);
       }
     }
   };
 
-  render() {
-    const { volume, flipped } = this.props;
-
-    return (
-      <div className={flipped ? 'volume-slider-compact volume-slider-compact--flipped' : 'volume-slider-compact'}>
-        <Slider
-          min={0}
-          max={100}
-          value={volume}
-          onChange={this.onVolumeInputChange}
-          aria-label="Volume"
-	  disabled={this.props.disabled}
-        />
-        <div className='volume-slider-compact__muter' onClick={this.onToggle}>
-          {volume === 0 ? (
-            <VolumeOff
-              style={{ ...SPEAKER_ICON_SIZE, transform: 'translateX(1px)' }}
-            />
-          ) : volume <= 40 ? (
-            <VolumeDown
-              style={{ ...SPEAKER_ICON_SIZE, transform: 'translateX(-0.5px)' }}
-            />
-          ) : (
-            <VolumeUp
-              style={{ ...SPEAKER_ICON_SIZE, transform: 'translateX(1px)' }}
-            />
-          )}
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div
+      ref={containerRef}
+      className={flipped ? 'volume-slider-compact volume-slider-compact--flipped' : 'volume-slider-compact'}
+      role="group"
+      aria-label="Volume control"
+    >
+      <Slider
+        min={0}
+        max={100}
+        value={volume}
+        onChange={onVolumeInputChange}
+        aria-label="Volume"
+        disabled={disabled}
+      />
+      <button
+        className='volume-slider-compact__muter'
+        onClick={onToggle}
+        aria-label={volume === 0 ? "Unmute" : "Mute"}
+        disabled={disabled}
+        type="button"
+      >
+        {volume === 0 ? (
+          <VolumeOff
+            style={{ ...SPEAKER_ICON_SIZE, transform: 'translateX(1px)' }}
+          />
+        ) : volume <= 40 ? (
+          <VolumeDown
+            style={{ ...SPEAKER_ICON_SIZE, transform: 'translateX(-0.5px)' }}
+          />
+        ) : (
+          <VolumeUp
+            style={{ ...SPEAKER_ICON_SIZE, transform: 'translateX(1px)' }}
+          />
+        )}
+      </button>
+    </div>
+  );
 }
+
+VolumeSliderCompact.propTypes = {
+  /** Current volume value */
+  volume: PropTypes.number.isRequired,
+  /** Handler for when volume is changed */
+  onVolumeChanged: PropTypes.func.isRequired,
+  /** Flip the order of the slider and icon */
+  flipped: PropTypes.bool,
+  disabled: PropTypes.bool,
+};
+
+VolumeSliderCompact.defaultProps = {
+  volume: 100,
+  flipped: false,
+};
 
 export default VolumeSliderCompact;
